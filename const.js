6edcdc27d67ab6fa2ec39b2f3bb3c590f059233f
unknown <Stanislav>
2022-06-02 09:52:20 +0300

    One commit

---
+++ const.js
@@ -0,0 +1,20 @@
+const commands = `
+/start - Перезапустить бота
+/help - Помощь
+/course - курсы
+`
+const text1 = `
+Текст для проверки обработчика <a href="https://youtube.com">ссылка на ютюб</a>
+`
+
+const text2 = `
+Текст для проверки обработчика <a href="https://vk.com">ссылка на vk</a>
+`
+const text3 = `
+Текст для проверки обработчика <a href="https://kwork.ru">ссылка на кворк</a>
+`
+
+module.exports.commands = commands
+module.exports.text1 = text1
+module.exports.text2 = text2
+module.exports.text3 = text3
